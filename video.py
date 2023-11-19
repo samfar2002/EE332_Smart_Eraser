528f6c0bad96b5db37b9a81ad6c868f6f00108e3
Enya Brett <enyabrett2025@u.northwestern.edu>
2023-11-19 17:14:11 -0600

    Added folders

--- video.py
+++ video.py
@@ -6,12 +6,12 @@ def decomposeVideo(file_path):
 
 
 
-def composeVideo(image_folder, file_path):
+def composeVideo(image_folder, file_path, fps):
     images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
 
-    video = cv2.VideoWriter(file_path, 0, 10, (width,height))
+    video = cv2.VideoWriter(file_path, 0, fps, (width,height))
 
     for image in images:
         video.write(cv2.imread(os.path.join(image_folder, image)))
