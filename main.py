317af7895c2ee40a042b6ce71eba648b77845711
Enya Brett <enyabrett2025@u.northwestern.edu>
2023-11-20 19:00:47 -0600

    Finishing tracking (still kinda patchy)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import video
 import tracking
+import os
 
 video.decomposeVideo("Original.avi", "original_frames", 400)
 
-tracking.find_pen()
+for file in os.listdir("original_frames"):
+    tracking.find_pen(file)
+
+video.composeVideo("original_frames", "result.avi", 15)
--- tracking.py
+++ tracking.py
@@ -1,25 +1,25 @@
 import cv2
 import numpy as np
 
-def find_pen():
-    image = cv2.imread("original_frames\\frame0000.jpg")
+def find_pen(input_frame):
+    image = cv2.imread("original_frames\\" + input_frame)
     hgt, wid, license = np.shape(image)
     xmin = wid
     ymin = hgt
     xmax = 0
-    ymax = 60
+    ymax = 0
 
     for x in range(wid):
         for y in range(60):
-            r = image[y][x][2]
-            g = image[y][x][0]
-            b = image[y][x][1]
+            r = int(image[y][x][2])
+            g = int(image[y][x][0])
+            b = int(image[y][x][1])
             if (r+g+b) == 0:
                 normalized_r = 0
             else:
                 normalized_r = r/(r+g+b)
                 
-            if normalized_r > 0.5:
+            if normalized_r > 0.7:
                 if x < xmin:
                     xmin = x
                 if y < ymin:
@@ -30,4 +30,6 @@ def find_pen():
                     ymax = y
     
     cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (255,255,255))
-    cv2.imwrite("result.jpg", image)
+    cv2.imwrite("original_frames\\" + input_frame, image)
+
+    return (xmin + xmax)/2
