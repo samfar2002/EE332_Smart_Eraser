64b4d7ae9aab3ae541ff0ed5e70e82d599d764f2
samfar2002 <107648681+samfar2002@users.noreply.github.com>
2023-11-23 00:12:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ candidates = pattern.find_candidates(frame_0, size_block, num_candidates, )
 
 
 for file in os.listdir("original_frames"):
-    tracking.find_pen(file)
+    x_cord = tracking.find_pen(file)
+    y_values = tracking.find_textures_to_replace(file,x_cord)
 
 video.composeVideo("original_frames", "result.avi", 15)
