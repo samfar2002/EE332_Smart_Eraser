3ca4e05c965c0893ecd41aa226d4cb891dc9f710
samfar2002 <107648681+samfar2002@users.noreply.github.com>
2023-11-23 00:11:07 -0600

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -32,4 +32,23 @@ def find_pen(input_frame):
     cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (255,255,255))
     cv2.imwrite("original_frames\\" + input_frame, image)
 
-    return (xmin + xmax)/2
+    return int((xmin + xmax)/2)
+
+def find_textures_to_replace(image, x_cord):
+    image = cv2.imread("original_frames/" + input_frame)
+    rows, cols, license = np.shape(image)
+    y_values = np.array([])
+    for y in range(60,rows):
+        r = image[y,x_cord,0]
+        g = image[y,x_cord,1]
+        b = image[y,x_cord,2]
+        
+        if (r+g+b) == 0:
+            normalized_r = 0
+        else:
+            normalized_r = r/(r+g+b)
+        
+        if normalized_r > 0.7:
+            y_values = np.append(y_values,y)
+            
+    return y_values
